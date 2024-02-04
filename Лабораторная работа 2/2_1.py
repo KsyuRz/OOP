d5a81164181defb4b2e30c8a498ab43055dafbb0
KsyuRz <148373566+KsyuRz@users.noreply.github.com>
2024-02-04 12:08:49 +0300

    Update 2_1.py

--- Лабораторная работа 2/2_1.py
+++ Лабораторная работа 2/2_1.py
@@ -11,28 +11,36 @@ BOOKS_DATABASE = [
     }
 ]
 
-# Создаем класс Книга
+
 class Book:
-    # Создаем конструктор с нужными атрибутами
-    def __init__(self, id_: int, name: str, pages: int):
-        self.id_ = id_
+    def __init__(self, id: int, name: str, pages: int):
+        if not isinstance(id, int):
+            raise TypeError('id_ должно быть типа int')
+        if id < 0:
+            raise ValueError('id_ должно быть больше 0')
+        self.id = id
+        if not isinstance(name, str):
+            raise TypeError('Название книги должно быть типа str')
         self.name = name
+        if not isinstance(pages, int):
+            raise TypeError('Количество страниц должно быть типа int')
+        if pages < 0:
+            raise ValueError('Количество страниц не должно быть отрицательным')
         self.pages = pages
 
-    # Создаем метод, который возвращает название книги
     def __str__(self):
         return f"Книга \"{self.name}\""
 
     def __repr__(self):
-        return f"Book(id_={self.id_}, name=\'{self.name}\', pages={self.pages})"
+        return f"Book(id_={self.id}, name=\'{self.name}\', pages={self.pages})"
 
 
 if __name__ == '__main__':
-    # инициализируем список книг
+    # Инициализация списка книг
     list_books = [
-        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+        Book(id=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
     ]
     for book in list_books:
-        print(book)  # проверяем метод __str__
+        print(book)  # Проверка метода __str__
 
-    print(list_books)  # проверяем метод __repr__
+    print(list_books)  # Проверка метода __repr__
